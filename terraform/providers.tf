54726c5c928a0d0a5454fb258ead3595b9452ad5
Noorunnsa <108316504+Noorunnsa@users.noreply.github.com>
2025-02-01 21:25:21 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket         = "flaskapp_statefiles"
+    key            = "statefiles/flaskapp.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf-state-lock"
+  }
   required_version = ">= 1.0.0"
   required_providers {
     aws = {
